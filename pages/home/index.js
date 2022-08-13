5bc66b5afdedfc1d56dba5d4640d3e83b7324ede
Funso Joba <hrfunsojoba@gmail.com>
2022-08-13 22:33:12 +0100

    navBar in progress

---
+++ pages/home/index.js
@@ -0,0 +1,14 @@
+import NavBar from "./navBar"
+
+
+const HomePage = () => {
+
+    return (
+        <div>
+            <NavBar />
+        <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ pages/home/navBar.js
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+
+
+const NavBar = ()=>{
+    return(
+        <div className="navBar">
+            <div className="navBar-logo">
+                <Image src="/DaventLogo.svg" alt="davent-logo" width={150} height={30} />
+            </div>
+            
+            <div className="navBar-menu">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="hambugger">
+                <Image src="/hambugger.svg" alt="hambugger" width={25} height={25} />
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
