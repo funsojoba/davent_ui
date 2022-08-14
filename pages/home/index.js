a148f4612ab4eea93bcd942496a004998002fef2
Funso Joba <hrfunsojoba@gmail.com>
2022-08-14 01:05:59 +0100

    section 4 home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -20,15 +20,47 @@ const HomePage = () => {
             </div>
 
             <div className="section-two">
-                <div className="section-two-text">
-                    <h1>What is Davent?</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
+                <div className="section-two-text flex-child">
+                    <h1 className="title-text">What is Davent?</h1>
+                    <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
                     <div className="buttons">
-                        <a id="link-hover" className="signup">Get Started</a>
+                        <a id="link-hover" className="get-started">Get Started</a>
                     </div>
                 </div>
-                <div className="section-two-image">
-                    <img src="/section-two.png" alt="davent-logo"  />
+                <div className="section-two-image flex-child">
+                    <div className="image">
+                        <img src="/section-two.png" alt="davent-logo"  />
+                    </div>
+                </div>
+            </div>
+            
+            <div className="section-three">
+                <div className="section-two-image flex-child">
+                    <div className="image">
+                        <img src="/section-three.png" alt="davent-logo"  />
+                    </div>
+                </div>
+                <div className="section-two-text flex-child">
+                    <h1 className="title-text">Get it done with ease</h1>
+                    <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
+                    <div className="buttons">
+                        <a id="link-hover" className="get-started">Get Started</a>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="section-two">
+                <div className="section-two-text flex-child">
+                    <h1 className="title-text">Seamless service</h1>
+                    <p className="paragraph">Get to know about the events happening around you, stay informed blah blah blah</p>
+                    <div className="buttons">
+                        <a id="link-hover" className="get-started">Get Started</a>
+                    </div>
+                </div>
+                <div className="section-two-image flex-child">
+                    <div className="image">
+                        <img src="/section-four.png" alt="davent-logo"  />
+                    </div>
                 </div>
             </div>
         </div>
