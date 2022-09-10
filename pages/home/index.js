5915b55242c6d498f9d7b9250107a08bc8cb192d
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2022-09-10 17:25:16 +0100

    signup page design

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,6 @@
 import NavBar from "./navBar"
 import Image from 'next/image'
+import ScrollAnimation from 'react-animate-on-scroll';
 
 
 const HomePage = () => {
@@ -8,96 +9,108 @@ const HomePage = () => {
         <div className="home-container">
             <NavBar />
             
-            <div className="section-one">
-                <div className="section-one-text">
-                    <h1>DAVENT</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
-                    <div className="buttons">
-                        <a id="link-hover" className="login">Log In</a>
-                        <a id="link-hover" className="signup">Sign Up</a>
+            <ScrollAnimation animateIn="fadeIn">
+                <div className="section-one">
+                    <div className="section-one-text">
+                        <h1>DAVENT</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
+                        <div className="buttons">
+                            <a id="link-hover" href="/signup" className="login">Log In</a>
+                            <a id="link-hover" className="signup">Sign Up</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <div className="section-two">
-                <div className="section-two-text flex-child">
-                    <h1 className="title-text">What is Davent?</h1>
-                    <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
-                    <div className="buttons">
-                        <a id="link-hover" className="get-started">Get Started</a>
+            </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeIn">
+                <div className="section-two">
+                    <div className="section-two-text flex-child">
+                        <h1 className="title-text">What is Davent?</h1>
+                        <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
+                        <div className="buttons">
+                            <a id="link-hover" className="get-started">Get Started</a>
+                        </div>
+                    </div>
+                    <div className="section-two-image flex-child">
+                        <div className="image">
+                            <img src="/section-two.png" alt="davent-logo"  />
+                        </div>
                     </div>
                 </div>
-                <div className="section-two-image flex-child">
-                    <div className="image">
-                        <img src="/section-two.png" alt="davent-logo"  />
-                    </div>
-                </div>
-            </div>
+            </ScrollAnimation>
             
-            <div className="section-three">
-                <div className="section-two-image flex-child">
-                    <div className="image">
-                        <img src="/section-three.png" alt="davent-logo"  />
+            <ScrollAnimation animateIn="fadeIn">
+                <div className="section-three">
+                    <div className="section-two-image flex-child">
+                        <div className="image">
+                            <img src="/section-three.png" alt="davent-logo"  />
+                        </div>
+                    </div>
+                    <div className="section-two-text flex-child">
+                        <h1 className="title-text">Get it done with ease</h1>
+                        <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
+                        <div className="buttons">
+                            <a id="link-hover" className="get-started">Get Started</a>
+                        </div>
                     </div>
                 </div>
-                <div className="section-two-text flex-child">
-                    <h1 className="title-text">Get it done with ease</h1>
-                    <p className="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa sagittis sit odio pellentesque. Risus eu pharetra.</p>
-                    <div className="buttons">
-                        <a id="link-hover" className="get-started">Get Started</a>
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="fadeIn">
+                <div className="section-two">
+                    <div className="section-two-text flex-child">
+                        <h1 className="title-text">Seamless service</h1>
+                        <p className="paragraph">Get to know about the events happening around you, stay informed blah blah blah</p>
+                        <div className="buttons">
+                            <a id="link-hover" className="get-started">Get Started</a>
+                        </div>
+                    </div>
+                    <div className="section-two-image flex-child">
+                        <div className="image">
+                            <img src="/section-four.png" alt="davent-logo"  />
+                        </div>
                     </div>
                 </div>
-                
-            </div>
-            <div className="section-two">
-                <div className="section-two-text flex-child">
-                    <h1 className="title-text">Seamless service</h1>
-                    <p className="paragraph">Get to know about the events happening around you, stay informed blah blah blah</p>
-                    <div className="buttons">
-                        <a id="link-hover" className="get-started">Get Started</a>
-                    </div>
-                </div>
-                <div className="section-two-image flex-child">
-                    <div className="image">
-                        <img src="/section-four.png" alt="davent-logo"  />
-                    </div>
-                </div>
-            </div>
+            </ScrollAnimation>
 
             <div className="footer">
-                <div className="about-us">
-                    <p>About us</p>
-                    <ul>
-                        <li><a href="#">Users</a></li>
-                        <li><a href="#">Admin</a></li>
-                        <li><a href="#">Event</a></li>
-                        <li><a href="#">Organization</a></li>
-                    </ul>
-                </div>
-                
-                <div className="about-us">
-                    <p>Policies</p>
-                    <ul>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Data Protection</a></li>
-                    </ul>
-                </div>
-                
-                <div className="about-us">
-                    <p>Developers</p>
-                    <ul>
-                        <li><a href="#">API Docs</a></li>
-                    </ul>
-                </div>
-                
-                <div className="about-us">
-                    <p>Contact Us</p>
-                    <ul>
-                        <li>1342 Helis St. Alen Avenue Sanfrasisco, LA</li>
-                        <li>+1 234 5678 0</li>
-                        <li>help@davent.com</li>
-                    </ul>
-                </div>
+                <ScrollAnimation animateIn="fadeIn">
+                    <div className="about-us">
+                        <p>About us</p>
+                        <ul>
+                            <li><a href="#">Users</a></li>
+                            <li><a href="#">Admin</a></li>
+                            <li><a href="#">Event</a></li>
+                            <li><a href="#">Organization</a></li>
+                        </ul>
+                    </div>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeIn">
+                    <div className="about-us">
+                        <p>Policies</p>
+                        <ul>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Data Protection</a></li>
+                        </ul>
+                    </div>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeIn">
+                    <div className="about-us">
+                        <p>Developers</p>
+                        <ul>
+                            <li><a href="#">API Docs</a></li>
+                        </ul>
+                    </div>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeIn">
+                    <div className="about-us">
+                        <p>Contact Us</p>
+                        <ul>
+                            <li>1342 Helis St. Alen Avenue Sanfrasisco, LA</li>
+                            <li>+1 234 5678 0</li>
+                            <li>help@davent.com</li>
+                        </ul>
+                    </div>
+                </ScrollAnimation>
             </div>
         </div>
     );
---
+++ pages/signup/index.js
@@ -0,0 +1,82 @@
+import NavBar from "../home/navBar"
+import Image from 'next/image'
+
+
+const HomePage = () => {
+
+    return (
+        <div className="home-container">
+            <NavBar />
+            <div className="sign-up-container">
+                <div className="sign-up-one">
+                    <div className="navBar-logo">
+                        <Image src="/DaventLogo.svg" alt="davent-logo" width={450} height={90} />
+                    </div>
+                </div>
+                <div className="sign-up-two">
+                    <div className="sign-up-form">
+                        <form>
+                            <div className="form-group">
+                                <div className="form-group-item">
+                                    <label for="firstName" className="label-name-white">First Name</label>
+                                    <input className="form-input" type="text" name="first_name" placeholder="John" />
+                                </div>
+                                <div className="form-group-item">
+                                    <label for="lastName" className="label-name-white">Last Name</label>
+                                    <input className="form-input" type="text" id="lastName" name="last_name" placeholder="Doe" />
+                                </div>
+                            </div>
+                            
+                            <div className="form-group">
+                                <div className="form-group-item">
+                                    <label for="phoneNumber" className="label-name-white">Phone Number</label>
+                                    <input className="form-input" type="tel" name="phone_number" placeholder="+2346907956" />
+                                </div>
+                                <div className="form-group-item">
+                                    <label for="email" className="label-name-white">Email</label>
+                                    <input className="form-input" type="email" id="email" name="email" placeholder="example@email.com" />
+                                </div>
+                            </div>
+                            
+                            <div className="form-group">
+                                <div className="form-group-item">
+                                    <label for="password1" className="label-name-white">Password</label>
+                                    <input className="form-input" id="password1" type="password" name="password" placeholder="*********" />
+                                </div>
+                                <div className="form-group-item">
+                                    <label for="password2" className="label-name-white">Repeat Password</label>
+                                    <input className="form-input" id="password2" type="password" name="password" placeholder="*********" />
+                                </div>
+                            </div>
+                            
+                            <div className="form-group">
+                                <div className="form-group-item">
+                                    <label for="cityOfResidence" className="label-name-white">City of Residence</label>
+                                    <input className="form-input" id="cityOfResidence" type="text" name="city" placeholder="Lekki" />
+                                </div>
+                                <div className="form-group-item">
+                                    <label for="stateOfResidence" className="label-name-white">Last Name</label>
+                                    <input className="form-input" id="stateOfResidence" type="text" name="state" placeholder="Lagos" />
+                                </div>
+                            </div>
+                            
+                            <div className="form-group">
+                                <div className="form-group-long">
+                                    <label for="country" className="label-name-white">Country</label>
+                                    <input className="form-input" type="text" name="country" placeholder="Nigeria" />
+                                </div>
+                            </div>
+                            
+                            <div className="form-group">
+                                <button className="yellow-button">SIGN UP</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
